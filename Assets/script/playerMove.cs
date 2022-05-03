00af7ea62d0869b98970147596b1d1707c4ebcea
kishwerjabeen <kishwerjabeen90@gmail.com>
2022-05-03 22:00:08 +0500

    plan view set responsive

---
+++ Assets/script/playerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    public float planSpeed;
+    public float padding = 0.8f;
+    float minX;
+    float maxX;
+    float minY;
+    float maxY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindBoundaries();
+    }
+
+    //First make funtion of boundries and call it in start() 
+    void FindBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        minX = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        maxX = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        minY = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        maxY = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * planSpeed;
+        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * planSpeed;
+
+        float newXpos = Mathf.Clamp(transform.position.x + deltaX, minX, maxX);
+        float newYpos = Mathf.Clamp(transform.position.y + deltaY, minY, maxY);
+
+        transform.position = new Vector2(newXpos, newYpos);
+    }
+}
