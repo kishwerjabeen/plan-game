6719662864be320a41f872443d895830a0e20e0f
kishwerjabeen <kishwerjabeen90@gmail.com>
2022-05-04 00:34:52 +0500

    player shoot

---
+++ Assets/script/shooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    public GameObject playerBullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // Quaternion.identity use when we dont wont to change position 
+            Instantiate(playerBullet, transform.position, Quaternion.identity);
+        }
+    }
+}
