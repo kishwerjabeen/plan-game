c6dec0d79bc60635260c4446e61e456c52fac3a5
Kishwar Jabeen <kishwerjabeen90@gmail.com>
2022-05-12 11:17:02 +0500

    Flash Muzzles

--- Assets/script/shooting.cs
+++ Assets/script/shooting.cs
@@ -8,13 +8,14 @@ public class shooting : MonoBehaviour
     public Transform spawnPointRight;
     public Transform spawnPointLeft;
     public float bulletSpawnTime = 1f;
+    public GameObject flash;
 
     // Start is called before the first frame update
     void Start()
     {
+        flash.SetActive(false);
         StartCoroutine(Shoot());
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -39,6 +40,10 @@ public class shooting : MonoBehaviour
         {
             yield return new WaitForSeconds(bulletSpawnTime);
             Fire();
+            flash.SetActive(true);
+            yield return new WaitForSeconds(0.08f);
+            flash.SetActive(false);
+
         }
 
 
