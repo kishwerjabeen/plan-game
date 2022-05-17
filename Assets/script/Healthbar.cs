925d773a01af248bb3b074df88293472680a6658
Kishwar Jabeen <kishwerjabeen90@gmail.com>
2022-05-17 09:25:43 +0500

    Healthbar destroy but not process

---
+++ Assets/script/Healthbar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthbar : MonoBehaviour
+{
+    public Transform bar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Setsize(0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Setsize(float size)
+    {
+        bar.localScale = new Vector2(size, 1f);
+    }
+}
--- Assets/script/enemy.cs
+++ Assets/script/enemy.cs
@@ -7,16 +7,31 @@ public class enemy : MonoBehaviour
     public Transform gunPointLeft;
     public Transform gunPointRight;
     public GameObject enemyBullet;
-    public float enemyBulletSpawnTime = 0.5f;
     public GameObject enemyFlash;
     public GameObject enemyExplosionPrefeb;
+    //healthbar jo script ka name ha wohi name daygae
+
+    public Healthbar healthbar;
+
+
+    public float enemyBulletSpawnTime = 0.5f;
+    
+   // healthbar 
+    public float health = 10f;
+
+    float barSize = 1f;
+    float damage = 0;
 
 
     // Start is called before the first frame update
+    
     void Start()
     {
         enemyFlash.SetActive(false);
         StartCoroutine(EnemyShooting());
+        // healthbar
+        damage = barSize / health;
+        healthbar.Setsize(barSize);
     }
 
     // Update is called once per frame
@@ -28,13 +43,35 @@ public class enemy : MonoBehaviour
     {
         if (collision.tag== "PlayerBullet")
         {
-            Destroy(gameObject);
-            GameObject enemyExplosion = Instantiate(enemyExplosionPrefeb, transform.position, Quaternion.identity);
-         // destroy copy
-            Destroy(enemyExplosion, 0.4f);
+            DamageHealthbar();
+            //bullet destroy 
+            Destroy(collision.gameObject);
+            if(health <= 0)
+            {
+                Destroy(gameObject);
+                GameObject enemyExplosion = Instantiate(enemyExplosionPrefeb, transform.position, Quaternion.identity);
+                // destroy copy
+                Destroy(enemyExplosion, 0.4f);
+            }
+
+
+
+          
         }
         
     }
+    //healthbar
+    void DamageHealthbar()
+    {
+        if (health > 0)
+        {
+            health -= 1;
+            barSize = barSize - damage;
+        }
+    }
+
+
+
     void EnemyFire()
     {
         Instantiate(enemyBullet, gunPointLeft.position, Quaternion.identity);
